5d4cecf1ec2bffbea597ad8dc1ad5f51e956afb7
Jonathan <mcjhona069@gmail.com>
2020-11-11 21:33:39 -0600

    Template mostar errores y mensajes

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -9,7 +9,7 @@ import { NgForm } from '@angular/forms';
 export class TemplateComponent implements OnInit {
 
   usuario = {
-    nombre: 'Jonathan',
+    nombre: 'Jonatha',
     apellido: 'Pérez',
     correo: 'mcjhona069@gmail.com'
   }
@@ -24,6 +24,15 @@ export class TemplateComponent implements OnInit {
 
   guardar( forma: NgForm ){
     console.log(forma);
+
+    if ( forma.invalid ){
+
+      Object.values(forma.controls).forEach(control =>{
+        control.markAsTouched();
+      });
+      return;
+    }
+
     console.log(forma.value);
   }
 
